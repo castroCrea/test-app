1e962b53f533df8ab95f7da14a44dfc152ba69c3
castroCrea <castro.crea@gmail.com>
2021-04-08 10:58:01 +0200

    ✨ Persist data

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,25 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import noteReducer from './note/noteSlices';
 
-export const store = configureStore({
-  reducer: {
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducers = combineReducers({
     note: noteReducer,
-  },
 });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+});
+
+
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
